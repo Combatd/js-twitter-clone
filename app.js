ced810d5c69516969fc9fcb1fadac28961bbf766
Combatd <combatdstar@gmail.com>
2019-11-16 11:29:03 -0800

    connect app.js to db with mongoose

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const db = require("./confg/keys").mongoURI; 
+
+
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then( () => console.log("Connected to mongoDB"))
+    .catch(err => console.log(err));
+
 
 app.get('/', (req, res) => {
     res.send('home route nodemon')
