1c51237ce437a509e9526835856300b8feb5da06
Combatd <combatdstar@gmail.com>
2019-11-16 15:14:22 -0800

    add users login route

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -30,18 +30,59 @@ router.post('/register', (req, res) => {
                     newUser.password = hash;
                     newUser.save()
                         .then(user => res.json(user))
-                        .catch(err => console.log(err));
-
-                    
+                        .catch(err => console.log(err));                  
                 });
             });
 
-
         }
-
         
     })
 });
+
+router.post('/login', async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    try {
+        const foundUser = await User.findOne( {email} )
+        console.log(req.body);
+        console.log(foundUser, " <- foundUser");
+
+        if (foundUser) {
+            
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if (isMatch) {
+                        res.json({ msg: 'Success' });
+                    } else {
+                        return res.status(400).json({ password: 'Incorrect password' });
+                    }
+                })
+
+        }
+
+    } catch (err) {
+        console.log(err);
+        res.send(err);
+    }
+
+    // User.findOne({ email })
+    //     .then(user => {
+    //         if (!user) {
+    //             return res.status(404).json({ email: 'This user does not exist' });
+    //         }
+
+    //         bcrypt.compare(password, user.password)
+    //             .then(isMatch => {
+    //                 if (isMatch) {
+    //                     res.json({ msg: 'Success' });
+    //                 } else {
+    //                     return res.status(400).json({ password: 'Incorrect password' });
+    //                 }
+    //             })
+    //     })
+})
+
 
 router.get('/test', (req, res) => {
     res.json({ message: "this is the user route"} );
