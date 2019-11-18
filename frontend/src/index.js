51da6c66ddeb83ef347a60a8925b84071c9c013d
Combatd <combatdstar@gmail.com>
2019-11-17 17:34:00 -0800

    require dependencies in frontend for react and redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import axios from 'axios';
 
 // We will create this component shortly
 import Root from './components/Root';
