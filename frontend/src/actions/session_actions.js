01328fe8b35a2ce63ec025d568946d2e494b28c7
Combatd <combatdstar@gmail.com>
2019-11-17 17:41:13 -0800

    format singup in src/actions/session_actions.js

--- frontend/src/actions/session_actions.js
+++ frontend/src/actions/session_actions.js
@@ -30,11 +30,10 @@ export const logoutUser = () => ({
 });
 
 // Upon signup, dispatch the approporiate action depending on which type of response we receieve from the backend
-export const signup = user => dispatch => (
-    APIUtil.signup(user).then(() => (
-        dispatch(receiveUserSignIn())
-    ), err => (
-        dispatch(receiveErrors(err.response.data))
+export const signup = (user) => dispatch => (
+    APIUtil.signup(user)
+    .then(() => (dispatch(receiveUserSignIn())) 
+    .catch(err => (dispatch(receiveErrors(err.response.data)))
     ))
 );
 
